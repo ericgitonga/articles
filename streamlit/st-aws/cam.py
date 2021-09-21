f791b1597581fc170a37abd66461121aded5d8d6
Eric Gitonga <gitonga@gmail.com>
2021-09-21 04:40:30 +0300

    refactor to pure capture

--- streamlit/st-aws/cam.py
+++ streamlit/st-aws/cam.py
@@ -9,25 +9,18 @@ Created on Tuesday Sep 21 04:00:00 2021
 import streamlit as st
 import cv2
 
-st.title("Capture image using webcam")
 
-opencam = cv2.VideoCapture(0)
-if not opencam.isOpened():
-    print("Webcam inactive")
+def capture_image():
+    opencam = cv2.VideoCapture(0)
+    if not opencam.isOpened():
+        st.write("Webcam inactive")
 
-frame_count = 0
-while(True):
-    ret, frame = opencam.read()
-    frame_count += 1
-#    print(frame_count)
-    if frame_count == 10:
-        output_image = frame
-        break
-#    cv2.imshow("preview", frame)
-#    if cv2.waitKey(1) & 0xFF == ord("q"):
-#        break
+    frame_count = 0
+    while(True):
+        ret, frame = opencam.read()
+        frame_count += 1
+        if frame_count == 50:
+            break
 
-opencam.release()
-cv2.destroyAllWindows()
-
-st.image(output_image)
+    opencam.release()
+    cv2.destroyAllWindows()
