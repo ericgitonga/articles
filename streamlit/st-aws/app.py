23e08c80d7c0af9dbb58c45762a06deff49d8a6c
Eric Gitonga <gitonga@gmail.com>
2021-09-22 12:21:37 +0300

    fix indent issues

--- streamlit/st-aws/app.py
+++ streamlit/st-aws/app.py
@@ -15,27 +15,27 @@ import io
 from PIL import Image
 
 with streamlit_analytics.track():
-	image_file = st.sidebar.file_uploader("Upload image")
+    image_file = st.sidebar.file_uploader("Upload image")
 
-	if st.sidebar.button("Capture webcam image"):
-    	read_image = capture_image()
-    	buffer_image = io.BytesIO()
-    	Image.fromarray(read_image).save(buffer_image, format="JPEG")
-    	image_file = io.BytesIO(buffer_image.getvalue())
+    if st.sidebar.button("Capture webcam image"):
+        read_image = capture_image()
+        buffer_image = io.BytesIO()
+        Image.fromarray(read_image).save(buffer_image, format="JPEG")
+        image_file = io.BytesIO(buffer_image.getvalue())
 
-	if image_file is not None:
-    	col1, col2 = st.columns(2)
-    	with col1:
-        	st.markdown("## Image From User")
-        	st.image(image_file)
+    if image_file is not None:
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("## Image From User")
+            st.image(image_file)
 
-	    s3 = boto3.resource("s3")
-    	bucket = s3.Bucket("st-to-s3")
-    	out_file = str(uuid.uuid4().hex)
-    	obj = bucket.Object("images/" + out_file + ".jpg")
-    	obj.upload_fileobj(image_file, ExtraArgs={"ACL": "public-read"})
+        s3 = boto3.resource("s3")
+        bucket = s3.Bucket("st-to-s3")
+        out_file = str(uuid.uuid4().hex)
+        obj = bucket.Object("images/" + out_file + ".jpg")
+        obj.upload_fileobj(image_file, ExtraArgs={"ACL": "public-read"})
 
-    	s3_url = "https://st-to-s3.s3.eu-west-2.amazonaws.com/images/"
-    	with col2:
-	        st.markdown("## Image From S3")
-    	    st.image(s3_url + out_file + ".jpg")
+        s3_url = "https://st-to-s3.s3.eu-west-2.amazonaws.com/images/"
+        with col2:
+            st.markdown("## Image From S3")
+            st.image(s3_url + out_file + ".jpg")
